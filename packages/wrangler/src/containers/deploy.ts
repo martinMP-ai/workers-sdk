2402309c72195984e346bf192520bc7ad046ffcb
emily-shen <eshen@cloudflare.com>
2025-08-12 18:11:50 +0100

    accept number[] in rollout_step_percentage too

--- packages/wrangler/src/containers/deploy.ts
+++ packages/wrangler/src/containers/deploy.ts
@@ -41,6 +41,7 @@ import type {
 	CreateApplicationRequest,
 	ModifyApplicationRequestBody,
 	Observability as ObservabilityConfiguration,
+	RolloutStepRequest,
 } from "@cloudflare/containers-shared";
 
 function mergeDeep<T>(target: T, source: Partial<T>): T {
@@ -378,7 +379,7 @@ const doAction = async (
 				application: ModifyApplicationRequestBody;
 				id: ApplicationID;
 				name: ApplicationName;
-				rollout_step_percentage?: number;
+				rollout_step_percentage: number | number[];
 				rollout_kind: CreateApplicationRolloutRequest.kind;
 		  }
 ) => {
@@ -447,41 +448,39 @@ const doAction = async (
 			);
 		}
 
-		if (action.rollout_step_percentage !== undefined) {
-			try {
-				await promiseSpinner(
-					RolloutsService.createApplicationRollout(action.id, {
-						description: "Progressive update",
-						strategy: CreateApplicationRolloutRequest.strategy.ROLLING,
-						target_configuration: action.application.configuration ?? {},
-						step_percentage: action.rollout_step_percentage,
-						kind: action.rollout_kind,
-					}),
-					{
-						message: `rolling out container version ${action.name}`,
-					}
-				);
-			} catch (err) {
-				if (!(err instanceof Error)) {
-					throw err;
+		try {
+			await promiseSpinner(
+				RolloutsService.createApplicationRollout(action.id, {
+					description: "Progressive update",
+					strategy: CreateApplicationRolloutRequest.strategy.ROLLING,
+					target_configuration: action.application.configuration ?? {},
+					...configRolloutStepsToAPI(action.rollout_step_percentage),
+					kind: action.rollout_kind,
+				}),
+				{
+					message: `rolling out container version ${action.name}`,
 				}
+			);
+		} catch (err) {
+			if (!(err instanceof Error)) {
+				throw err;
+			}
 
-				if (!(err instanceof ApiError)) {
-					throw new UserError(
-						`Unexpected error rolling out application ${action.name}:\n${err.message}`
-					);
-				}
-
-				if (err.status === 400) {
-					throw new UserError(
-						`Error rolling out application ${action.name} due to a misconfiguration:\n\n\t${formatError(err)}`
-					);
-				}
-
+			if (!(err instanceof ApiError)) {
 				throw new UserError(
-					`Error rolling out application ${action.name} due to an internal error (request id: ${err.body.request_id}): ${formatError(err)}`
+					`Unexpected error rolling out application ${action.name}:\n${err.message}`
 				);
 			}
+
+			if (err.status === 400) {
+				throw new UserError(
+					`Error rolling out application ${action.name} due to a misconfiguration:\n\n\t${formatError(err)}`
+				);
+			}
+
+			throw new UserError(
+				`Error rolling out application ${action.name} due to an internal error (request id: ${err.body.request_id}): ${formatError(err)}`
+			);
 		}
 
 		success(
@@ -531,3 +530,20 @@ export function cleanApplicationFromAPI(
 
 	return cleanedPreviousApp;
 }
+
+export const configRolloutStepsToAPI = (rolloutSteps: number | number[]) => {
+	if (typeof rolloutSteps === "number") {
+		return { step_percentage: rolloutSteps };
+	} else {
+		const output: RolloutStepRequest[] = [];
+		let index = 1;
+		for (const step of rolloutSteps) {
+			output.push({
+				step_size: { percentage: step },
+				description: `Step ${index} of ${rolloutSteps.length} applying to ${step}% of instances`,
+			});
+			index++;
+		}
+		return { steps: output };
+	}
+};
